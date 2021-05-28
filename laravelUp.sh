d89332e65253ba7b1e354895cfe588bf09d2fdee
Kobe Daniëls <57659711+KobeDaniels@users.noreply.github.com>
2021-05-28 14:36:10 +0200

    Laravel extra
    updated, with more details into laravel

--- laravelUp.sh
+++ laravelUp.sh
@@ -18,3 +18,13 @@ services:
 cd /home/${gebruikersnaam}/ftp
 chmod 777 -R ./upload/
 sudo docker run -d -p ${ip}:80:80 --volume=/home/${gebruikersnaam}/ftp/upload:/var/www/app/:z --name=${gebruikersnaam}-laravel laravel
+
+docker exec ${gebruikersnaam}-laravel bash -c 'cd /var/www/app ; chmod -R 777 storage/'
+
+docker exec ${gebruikersnaam}-laravel bash -c 'cd /var/www/app ; composer install'
+
+docker exec ${gebruikersnaam}-laravel bash -c 'cd /var/www/app ; php artisan key:generate'
+
+docker exec ${gebruikersnaam}-laravel bash -c 'cd /var/www/app ; php artisan migrate'
+
+docker exec ${gebruikersnaam}-laravel bash -c 'cd /var/www/app ; php artisan db:seed'
