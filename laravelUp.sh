9fddede239b8ba812aace559ccaa75b3ebd2f2fe
Alex Coulon <57891540+Alex-Coulon@users.noreply.github.com>
2021-05-28 09:19:29 +0200

    Create laravelUp.sh

---
+++ laravelUp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+gebruikersnaam=$1
+ip=$2
+
+
+echo "version: '3'
+services:
+  webserver:
+    container_name: php-apache2-mysql-main-${gebruikersnaam}
+    image: php:7.4-apache
+    volumes:
+     - ./upload:/var/www/html:z
+    ports:
+     - "${ip}:80:80"" > /home/${gebruikersnaam}/ftp/docker-compose.yml
+
+
+cd /home/${gebruikersnaam}/ftp
+chmod 777 -R ./upload/
+sudo docker run -d -p ${ip}:80:80 --volume=/home/${gebruikersnaam}/ftp/upload:/var/www/app/:z --name=${gebruikersnaam}-laravel laravel
